01716fe00a1b0e0b2f37c5a9a30f8077d61f5dcb
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-23 06:05:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,13 +4,15 @@
 hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
-  temp = 100
+  temp_value = 100
+  
   name_hash.each do |key, value|
     if value < temp
       temp = value
+      temp_key = key
     end
   end
-  temp
+  temp_key
 end  
 
 #key_for_min_value(hash)
