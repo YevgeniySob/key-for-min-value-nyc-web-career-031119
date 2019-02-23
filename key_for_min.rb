b6ebb33bec6b049f2d6d1a498574e4d55f4cb1db
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-23 06:07:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,9 @@
 hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
+  if name_hash.size == 0
+    nil
+  end
   temp_value = 100
   temp_key = ""
   name_hash.each do |key, value|
